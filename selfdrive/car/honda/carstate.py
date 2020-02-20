ada8b8226514a4e4116c9dbdafc3356eb040f987
Comma Device <device@comma.ai>
2020-02-20 02:54:48 +0000

    Post 0.7.3 fix 2

--- selfdrive/car/honda/carstate.py
+++ selfdrive/car/honda/carstate.py
@@ -221,7 +221,6 @@ class CarState(CarStateBase):
 
     # update prevs, update must run once per loop
     self.prev_cruise_buttons = self.cruise_buttons
-    self.prev_blinker_on = self.blinker_on
     self.prev_lead_distance = self.lead_distance
     self.prev_left_blinker_on = self.left_blinker_on
     self.prev_cruise_setting = self.cruise_setting
