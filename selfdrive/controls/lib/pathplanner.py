2c991fabfc717beebddd8543ef1da149fa10b341
kegman <8837066+kegman@users.noreply.github.com>
2020-10-31 21:36:23 -0400

    0.7.10 fixes part D

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -5,6 +5,7 @@ from selfdrive.swaglog import cloudlog
 from selfdrive.controls.lib.lateral_mpc import libmpc_py
 from selfdrive.controls.lib.drive_helpers import MPC_COST_LAT
 from selfdrive.controls.lib.lane_planner import LanePlanner
+from common.params import Params
 from selfdrive.kegman_conf import kegman_conf
 from common.numpy_fast import interp
 import cereal.messaging as messaging
