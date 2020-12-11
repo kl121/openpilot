03ba0f21e36606f6ef45c8dc1f1297f974fc1599
kegman <8837066+kegman@users.noreply.github.com>
2020-12-11 18:23:39 -0500

    Turn off audible Steer saturated alert

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -456,7 +456,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, Callable[[Any, messaging.SubMaster, boo
       "TAKE CONTROL",
       "Turn Exceeds Steering Limit",
       AlertStatus.userPrompt, AlertSize.mid,
-      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.chimePrompt, 1., 1., 1.),
+      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.none, 1., 1., 1.),
   },
 
   EventName.fanMalfunction: {
