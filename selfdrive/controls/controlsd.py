b9919c399889b12a6e6a0327b07acd11fdb7e6f6
Comma Device <device@comma.ai>
2021-03-17 03:53:59 +0000

    Post merge 0.8.2 fix Part E

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -65,7 +65,9 @@ class Controls:
       self.can_sock = messaging.sub_sock('can', timeout=can_timeout)
 
     # wait for one health and one CAN packet
-    hw_type = messaging.recv_one(self.sm['pandaState']).pandaType
+    hw_type = messaging.recv_one(self.sm.sock['pandaState']).pandaState.pandaType 
+
+    #hw_type = messaging.recv_one(self.sm['pandaState']).pandaType
     has_relay = hw_type in [PandaType.blackPanda, PandaType.uno, PandaType.dos]
 
     print("Waiting for CAN messages...")
