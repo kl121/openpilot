9b245f187de75a214fc84c06d203bd885551e264
hanabi95 <youngsoo.cha@gmail.com>
2021-03-19 19:26:46 +0900

    openpilot v0.8.3 release

--- selfdrive/ui/qt/offroad/onboarding.hpp
+++ selfdrive/ui/qt/offroad/onboarding.hpp
@@ -20,7 +20,7 @@ private:
   QStackedLayout* slayout;
 
   // Vector of bounding boxes for the a given training guide step. (minx, maxx, miny, maxy)
-  QVector<QVector<int>> boundingBox {{250, 930, 750, 900}, {280, 1280, 650, 950}, {330, 1130, 590, 900}, {910, 1580, 500, 1000}, {1180, 1300, 630, 720}, {290, 1050, 590, 960},
+  QVector<QVector<int>> boundingBox {{250, 930, 750, 900}, {280, 1280, 650, 950}, {330, 1130, 590, 900}, {910, 1580, 500, 1000}, {1180, 1300, 630, 720}, {290, 1050, 590, 960}, 
   {1090, 1240, 550, 660}, {1050, 1580, 250, 900}, {320, 1130, 670, 1020}, {1010, 1580, 410, 750}, {1040, 1500, 230, 1030}, {300, 1190, 590, 920}, {1050, 1310, 170, 870}, {950, 1530, 460, 770}, {190, 970, 750, 970}};
 
 signals:
@@ -40,10 +40,6 @@ private:
   QTextEdit *terms_text;
   QWidget *terms_screen();
   QWidget *training_screen();
-
-#ifndef QCOM
-  QWebEngineView* view;
-#endif
 
 signals:
   void onboardingDone();
