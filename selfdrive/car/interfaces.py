1a0b121df487f7e91eedcdeeae03024b9af27393
hanabi95 <youngsoo.cha@gmail.com>
2021-03-25 20:55:17 +0900

    Update some values

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -56,8 +56,8 @@ class CarInterfaceBase():
 
     # standard ALC params
     ret.steerControlType = car.CarParams.SteerControlType.torque
-    ret.steerMaxBP = [10., 18., 25.]
-    ret.steerMaxV = [0.9, 1., 1.1]
+    ret.steerMaxBP = [0.]
+    ret.steerMaxV = [1.]
     ret.minSteerSpeed = 0.
 
     # stock ACC by default
