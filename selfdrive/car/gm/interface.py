2491ad1cc91bd9831302c6bca0d133998b7f90b6
whrho <whrho@sparrowfasoo.com>
2021-04-15 15:50:19 +0900

    mroe generous steer-saturated limit little bit up lqr sacle (1950->1965)

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -50,7 +50,7 @@ class CarInterface(CarInterfaceBase):
     ret.centerToFront = ret.wheelbase * 0.4 # wild guess
     ret.lateralTuning.init('lqr')
 
-    ret.lateralTuning.lqr.scale = 1950.0
+    ret.lateralTuning.lqr.scale = 1965.0
     ret.lateralTuning.lqr.ki = 0.024
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -422,8 +422,8 @@ class Controls:
 
       if len(lat_plan.dPathPoints):
         # Check if we deviated from the path
-        left_deviation = actuators.steer > 0 and lat_plan.dPathPoints[0] < -0.1
-        right_deviation = actuators.steer < 0 and lat_plan.dPathPoints[0] > 0.1
+        left_deviation = actuators.steer > 0 and lat_plan.dPathPoints[0] < -0.125
+        right_deviation = actuators.steer < 0 and lat_plan.dPathPoints[0] > 0.125
 
         if left_deviation or right_deviation:
           self.events.add(EventName.steerSaturated)
