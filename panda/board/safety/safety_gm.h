92d4b2fe8343747817f4f7064b537c79f817bffb
hanabi95 <youngsoo.cha@gmail.com>
2021-04-17 18:30:08 +0900

    Update safety_gm.h

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -15,18 +15,32 @@ const int GM_MAX_GAS = 3072;
 const int GM_MAX_REGEN = 1404;
 const int GM_MAX_BRAKE = 350;
 const int GM_GAS_INTERCEPTOR_THRESHOLD = 458;  // (610 + 306.25) / 2ratio between offset and gain from dbc file
-const CanMsg GM_TX_MSGS[] = {{384, 0, 4}, {1033, 0, 7}, {1034, 0, 7}, {715, 0, 8}, {880, 0, 6}, {512, 0, 6}, // pt bus
-                             {161, 1, 7}, {774, 1, 8}, {776, 1, 7}, {784, 1, 2},   // obs bus
-                             {789, 2, 5},  // ch bus
+#define GM_GET_INTERCEPTOR(msg) (((GET_BYTE((msg), 0) << 8) + GET_BYTE((msg), 1) + ((GET_BYTE((msg), 2) << 8) + GET_BYTE((msg), 3)) / 2 ) / 2) // avg between 2 tracks
+
+// Safety-relevant CAN messages for Bolt EV
+#define MSG_RX_STEER      0x184   // RX from PSCM, for steering torque and status
+#define MSG_RX_WHEEL      0x34A   // RX from EBCM, for wheel speed
+#define MSG_RX_BUTTON     0x1E1   // RX from BCM, for Cruise Buttons
+#define MSG_RX_BRAKE      0xF1    // RX from EBCM, for Brake Position
+#define MSG_RX_GAS        0x1A1   // RX from EBCM, for Pedal Position
+
+#define MSG_TX_LKA        0x180   // TX by OP, for LKA commands
+#define MSG_TX_ALIVE      0x409   // TX by OP, for ASCM Alive, To do : We need to check if this message is used for Bolt EV or not.
+#define MSG_TX_ASCM       0x40A   // TX by OP, for ASCM, To do : We need to check if this message is used for Bolt EV or not.
+#define MSG_TX_ACC        0x370   // TX by OP, for ACC Status, To do : We need to check if this message is used for Bolt EV or not.
+#define MSG_TX_PEDAL_01   0x200   // TX by OP, for Pedal Interceptor
+#define MSG_TX_PEDAL_02   0x201   // TX by OP, for Pedal Interceptor
+
+const CanMsg GM_TX_MSGS[] = {{MSG_TX_LKA, 0, 4}, {MSG_TX_ALIVE, 0, 7}, {MSG_TX_ASCM, 0, 7}, {MSG_TX_ACC, 0, 6}, {MSG_TX_PEDAL_01, 0, 6}, // pt bus
                              {0x104c006c, 3, 3}, {0x10400060, 3, 5}};  // gmlan
 
 // TODO: do checksum and counter checks. Add correct timestep, 0.1s for now.
 AddrCheckStruct gm_rx_checks[] = {
-  {.msg = {{388, 0, 8, .expected_timestep = 100000U}}},
-  {.msg = {{842, 0, 5, .expected_timestep = 100000U}}},
-  {.msg = {{481, 0, 7, .expected_timestep = 100000U}}},
-  {.msg = {{241, 0, 6, .expected_timestep = 100000U}}},
-  {.msg = {{417, 0, 7, .expected_timestep = 100000U}}},
+  {.msg = {{MSG_RX_STEER, 0, 8, .expected_timestep = 100000U}}},
+  {.msg = {{MSG_RX_WHEEL, 0, 5, .expected_timestep = 100000U}}},
+  {.msg = {{MSG_RX_BUTTON, 0, 7, .expected_timestep = 100000U}}},
+  {.msg = {{MSG_RX_BRAKE, 0, 6, .expected_timestep = 100000U}}},
+  {.msg = {{MSG_RX_GAS, 0, 7, .expected_timestep = 100000U}}},
 };
 const int GM_RX_CHECK_LEN = sizeof(gm_rx_checks) / sizeof(gm_rx_checks[0]);
 
@@ -38,7 +52,7 @@ static int gm_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
   if (valid && (GET_BUS(to_push) == 0)) {
     int addr = GET_ADDR(to_push);
 
-    if (addr == 388) {
+    if (addr == MSG_RX_STEER) {
       int torque_driver_new = ((GET_BYTE(to_push, 6) & 0x7) << 8) | GET_BYTE(to_push, 7);
       torque_driver_new = to_signed(torque_driver_new, 11);
       // update array of samples
@@ -47,12 +61,12 @@ static int gm_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
 
     // sample speed, really only care if car is moving or not
     // rear left wheel speed
-    if (addr == 842) {
+    if (addr == MSG_RX_WHEEL) {
       vehicle_moving = GET_BYTE(to_push, 0) | GET_BYTE(to_push, 1);
     }
 
     // ACC steering wheel buttons
-    if (addr == 481) {
+    if (addr == MSG_RX_BUTTON) {
       int button = (GET_BYTE(to_push, 5) & 0x70) >> 4;
       switch (button) {
         case 2:  // resume
@@ -66,74 +80,87 @@ static int gm_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
     }
 
     // speed > 0
-    if (addr == 241) {
+    if (addr == MSG_RX_BRAKE) {
       // Brake pedal's potentiometer returns near-zero reading
       // even when pedal is not pressed
       brake_pressed = GET_BYTE(to_push, 1) >= 10;
     }
 
-    if (addr == 417) {
+    if (addr == MSG_RX_GAS) {
       gas_pressed = GET_BYTE(to_push, 6) != 0;
     }
 
-    // exit controls on regen paddle
-    if (addr == 189) {
-      bool regen = GET_BYTE(to_push, 0) & 0x20;
-      if (regen) {
-        controls_allowed = 1;
-      }
+    // Gas Interceptor Check
+    if ((addr == MSG_TX_PEDAL_02) && (len == 6)) {
+      gas_interceptor_detected = 1;
+      int gas_interceptor = GM_GET_INTERCEPTOR(to_push);
+      //gas_pressed = gas_interceptor > HONDA_GAS_INTERCEPTOR_THRESHOLD;
+      //gas_interceptor_prev = gas_interceptor;
     }
 
-    // Check if ASCM or LKA camera are online
+    // Check if LKA camera are online
     // on powertrain bus.
     // 384 = ASCMLKASteeringCmd
-    // 715 = ASCMGasRegenCmd
-    generic_rx_checks(((addr == 384) || (addr == 715)));
+    generic_rx_checks((addr == MSG_TX_LKA));
   }
   return valid;
 }
 
-// all commands: gas/regen, friction brake and steering
-// if controls_allowed and no pedals pressed
-//     allow all commands up to limit
-// else
-//     block all commands that produce actuation
+static bool gm_steering_check(int desired_torque) {
+  bool violation = false;
+  uint32_t ts = TIM2->CNT;
+
+  if (controls_allowed) {
+    // *** global torque limit check ***
+    violation |= max_limit_check(desired_torque, GM_MAX_STEER, -GM_MAX_STEER);
+
+    // *** torque rate limit check ***
+    violation |= driver_limit_check(desired_torque, desired_torque_last, &torque_driver,
+      GM_MAX_STEER, GM_MAX_RATE_UP, GM_MAX_RATE_DOWN,
+      GM_DRIVER_TORQUE_ALLOWANCE, GM_DRIVER_TORQUE_FACTOR);
+
+    // used next time
+    desired_torque_last = desired_torque;
+
+    // *** torque real time rate limit check ***
+    violation |= rt_rate_limit_check(desired_torque, rt_torque_last, GM_MAX_RT_DELTA);
+
+    // every RT_INTERVAL set the new limits
+    uint32_t ts_elapsed = get_ts_elapsed(ts, ts_last);
+    if (ts_elapsed > GM_RT_INTERVAL) {
+      rt_torque_last = desired_torque;
+      ts_last = ts;
+    }
+  }
+
+  // no torque if controls is not allowed
+  if (!controls_allowed && (desired_torque != 0)) {
+    violation = true;
+  }
+
+  // reset to 0 if either controls is not allowed or there's a violation
+  if (violation || !controls_allowed) {
+    desired_torque_last = 0;
+    rt_torque_last = 0;
+    ts_last = ts;
+  }
+
+  return violation;
+}
+
 
 static int gm_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
 
   int tx = 1;
   int addr = GET_ADDR(to_send);
 
-  if (!msg_allowed(to_send, GM_TX_MSGS, sizeof(GM_TX_MSGS)/sizeof(GM_TX_MSGS[0]))) {
+  if (!msg_allowed(to_send, GM_TX_MSGS, sizeof(GM_TX_MSGS)/sizeof(GM_TX_MSGS[0])) || relay_malfunction) {
     tx = 0;
-  }
-
-  if (relay_malfunction) {
-    tx = 0;
-  }
-
-  // disallow actuator commands if gas or brake (with vehicle moving) are pressed
-  // and the the latching controls_allowed flag is True
-  int pedal_pressed = brake_pressed_prev && vehicle_moving;
-  bool current_controls_allowed = controls_allowed && !pedal_pressed;
-
-  // BRAKE: safety check
-  if (addr == 789) {
-    int brake = ((GET_BYTE(to_send, 0) & 0xFU) << 8) + GET_BYTE(to_send, 1);
-    brake = (0x1000 - brake) & 0xFFF;
-    if (!current_controls_allowed) {
-      if (brake != 0) {
-        tx = 0;
-      }
-    }
-    if (brake > GM_MAX_BRAKE) {
-      tx = 0;
-    }
   }
 
   // GAS: Interceptor safety check
-  if (addr == 0x200) {
-    if (!current_controls_allowed) {
+  if ((addr == MSG_TX_PEDAL_01) && gas_interceptor_detected) {
+    if (!controls_allowed) {
       if (GET_BYTE(to_send, 0) || GET_BYTE(to_send, 1)) {
         tx = 0;
       }
@@ -141,65 +168,11 @@ static int gm_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   }
 
   // LKA STEER: safety check
-  if (addr == 384) {
+  if (addr == MSG_TX_LKA) {
     int desired_torque = ((GET_BYTE(to_send, 0) & 0x7U) << 8) + GET_BYTE(to_send, 1);
-    uint32_t ts = TIM2->CNT;
-    bool violation = 0;
     desired_torque = to_signed(desired_torque, 11);
 
-    if (current_controls_allowed) {
-
-      // *** global torque limit check ***
-      violation |= max_limit_check(desired_torque, GM_MAX_STEER, -GM_MAX_STEER);
-
-      // *** torque rate limit check ***
-      violation |= driver_limit_check(desired_torque, desired_torque_last, &torque_driver,
-        GM_MAX_STEER, GM_MAX_RATE_UP, GM_MAX_RATE_DOWN,
-        GM_DRIVER_TORQUE_ALLOWANCE, GM_DRIVER_TORQUE_FACTOR);
-
-      // used next time
-      desired_torque_last = desired_torque;
-
-      // *** torque real time rate limit check ***
-      violation |= rt_rate_limit_check(desired_torque, rt_torque_last, GM_MAX_RT_DELTA);
-
-      // every RT_INTERVAL set the new limits
-      uint32_t ts_elapsed = get_ts_elapsed(ts, ts_last);
-      if (ts_elapsed > GM_RT_INTERVAL) {
-        rt_torque_last = desired_torque;
-        ts_last = ts;
-      }
-    }
-
-    // no torque if controls is not allowed
-    if (!current_controls_allowed && (desired_torque != 0)) {
-      violation = 1;
-    }
-
-    // reset to 0 if either controls is not allowed or there's a violation
-    if (violation || !current_controls_allowed) {
-      desired_torque_last = 0;
-      rt_torque_last = 0;
-      ts_last = ts;
-    }
-
-    if (violation) {
-      tx = 0;
-    }
-  }
-
-  // GAS/REGEN: safety check
-  if (addr == 715) {
-    int gas_regen = ((GET_BYTE(to_send, 2) & 0x7FU) << 5) + ((GET_BYTE(to_send, 3) & 0xF8U) >> 3);
-    // Disabled message is !engaged with gas
-    // value that corresponds to max regen.
-    if (!current_controls_allowed) {
-      bool apply = GET_BYTE(to_send, 0) & 1U;
-      if (apply || (gas_regen != GM_MAX_REGEN)) {
-        tx = 0;
-      }
-    }
-    if (gas_regen > GM_MAX_GAS) {
+    if (gm_steering_check(desired_torque)) {
       tx = 0;
     }
   }
@@ -208,12 +181,40 @@ static int gm_tx_hook(CAN_FIFOMailBox_TypeDef *to_send) {
   return tx;
 }
 
+static void gm_init(int16_t param) {
+  UNUSED(param);
+  controls_allowed = false;
+  relay_malfunction_reset();
+  gas_interceptor_detected = 0;
+}
+
+static int gm_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
+  int bus_fwd = -1;
+  int bus_camera = 1; //camera PT can bus for grey panda and custom made harness
+  //int bus_radar = 1; // Radar can bus, Bolt EV doesn't need this can bus
+  //int bus_camera = 2; //vehicle PT can bus for comma ai harness
+  int bus_vehicle = 0; //vehicle PT can bus for comma ai harness
+  //int bus_chassis = 3; //vehicle Chassis can bus, Bolt EV doesn't need this can bus
+  int addr = GET_ADDR(to_fwd);
+
+  if (bus_num == 0) {
+    bus_fwd = 1;       //Forward all messages from vehicle
+  }
+
+  if (bus_num == 1) {
+    if (addr == MSG_TX_LKA) {
+      bus_fwd = -1;    //Block LKA message from camera
+    } else {
+    bus_fwd = 0;       //Forward all messages except LAK message
+  }
+}
+
 const safety_hooks gm_hooks = {
-  .init = nooutput_init,
+  .init = gm_init,
   .rx = gm_rx_hook,
   .tx = gm_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
-  .fwd = default_fwd_hook,
+  .fwd = gm_fwd_hook,
   .addr_check = gm_rx_checks,
   .addr_check_len = sizeof(gm_rx_checks) / sizeof(gm_rx_checks[0]),
 };
