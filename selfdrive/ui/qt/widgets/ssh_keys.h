2265a98ec7867484f802a10d0ea38f9b652dc10d
Vehicle Researcher <user@comma.ai>
2021-05-05 19:30:17 +0000

    openpilot v0.8.4 release

---
+++ selfdrive/ui/qt/widgets/ssh_keys.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <QPushButton>
+
+#include "widgets/controls.h"
+#include "selfdrive/hardware/hw.h"
+
+// SSH enable toggle
+class SshToggle : public ToggleControl {
+  Q_OBJECT
+
+public:
+  SshToggle() : ToggleControl("Enable SSH", "", "", Hardware::get_ssh_enabled()) {
+    QObject::connect(this, &SshToggle::toggleFlipped, [=](bool state) {
+      Hardware::set_ssh_enabled(state);
+    });
+  }
+};
+
+// SSH key management widget
+class SshControl : public AbstractControl {
+  Q_OBJECT
+
+public:
+  SshControl();
+
+private:
+  Params params;
+
+  QPushButton btn;
+  QLabel username_label;
+
+  void refresh();
+  void getUserKeys(const QString &username);
+};
