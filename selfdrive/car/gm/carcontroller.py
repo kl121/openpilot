b30f644f0365e15dff0ae7b29e9e3150ab4df3c5
hanabi95 <61353425+hanabi95@users.noreply.github.com>
2021-05-07 16:33:09 +0900

    Update carcontroller.py

--- selfdrive/car/gm/carcontroller.py
+++ selfdrive/car/gm/carcontroller.py
@@ -67,7 +67,7 @@ class CarController():
       gas = (1-zero) * actuators.gas + zero
       regen_brake = clip(actuators.brake, 0., zero)
       final_accel = gas - regen_brake
-      final_accel, self.accel_steady = accel_hysteresis(final_accel, self.accel_steady)
+      #final_accel, self.accel_steady = accel_hysteresis(final_accel, self.accel_steady)
       final_pedal = clip(final_accel, 0., 1.)
       if (frame % 4) == 0:
         idx = (frame // 4) % 4
