2803c7cae52e47c8cfc13ae797d84b67baffde8b
jc01rho <rkjnice@gmail.com>
2021-05-08 01:41:16 +0900

    testing to print batt LVL

--- selfdrive/ui/qt/sidebar.cc
+++ selfdrive/ui/qt/sidebar.cc
@@ -154,8 +154,9 @@ void Sidebar::update(const UIState &s) {
       {cereal::DeviceState::ThermalStatus::YELLOW, COLOR_WARNING},
       {cereal::DeviceState::ThermalStatus::RED, COLOR_DANGER},
       {cereal::DeviceState::ThermalStatus::DANGER, COLOR_DANGER}};
-  QString temp_val = QString("%1 °C").arg((int)s.scene.deviceState.getAmbientTempC());
-  temp->update(temp_val, temp_severity_map[s.scene.deviceState.getThermalStatus()], s.scene.deviceState.getBatteryVoltage().c_str());
+  QString temp_val = QString("BATT: %1 °C").arg((int)s.scene.deviceState.getAmbientTempC());
+  temp->update(temp_val, temp_severity_map[s.scene.deviceState.getThermalStatus()], QString("%1 %").arg((int)s.scene.deviceState.getBatteryVoltage()));
+
 
   static std::map<cereal::DeviceState::NetworkType, const char *> network_type_map = {
       {cereal::DeviceState::NetworkType::NONE, "--"},
