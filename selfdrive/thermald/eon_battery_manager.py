61b9c30e90550074a3d716a148b7d731d4f4c9b7
whrho <whrho@sparrowfasoo.com>
2021-05-08 21:13:02 +0900

    fix eon charging conditions

--- selfdrive/thermald/eon_battery_manager.py
+++ selfdrive/thermald/eon_battery_manager.py
@@ -5,14 +5,24 @@ BATT_PERC_MAX = 65
 
 def setEONChargingStatus(car_voltage_mV, batteryPercent) :
     if EON:
-        print("log purpose : entering setEONChargingStatus ")
+        if car_voltage_mV is None or batteryPercent is None :
+            HARDWARE.set_battery_charging(True)
+            return
+        print( "car_voltage_mV:",car_voltage_mV)
+        print( "batteryPercent:",batteryPercent)
+        print( "VBATT_PAUSE_CHARGING:",VBATT_PAUSE_CHARGING)
+        print("VBATT_PAUSE_CHARGING * 1e3:", VBATT_PAUSE_CHARGING * 1e3)
         if HARDWARE.get_battery_charging() :
-            print("log purpose : HARDWARE.get_battery_charging()  True ")
-            if batteryPercent > BATT_PERC_MAX or car_voltage_mV  < VBATT_PAUSE_CHARGING * 1e3 :
+            # print("log purpose : HARDWARE.get_battery_charging()  True ")
+            # if batteryPercent > BATT_PERC_MAX or car_voltage_mV  < VBATT_PAUSE_CHARGING * 1e3 :
+            if batteryPercent > BATT_PERC_MAX : #or car_voltage_mV < VBATT_PAUSE_CHARGING * 1e3:
                 print("log purpose : HARDWARE.set_battery_charging(False)  False ")
                 HARDWARE.set_battery_charging(False)
+                return
         else :
-            print("log purpose : HARDWARE.get_battery_charging()  False ")
-            if batteryPercent < BATT_PERC_MIN and car_voltage_mV  > VBATT_PAUSE_CHARGING * 1e3:
+            # print("log purpose : HARDWARE.get_battery_charging()  False ")
+            #if batteryPercent < BATT_PERC_MIN and car_voltage_mV  > VBATT_PAUSE_CHARGING * 1e3:
+            if batteryPercent < BATT_PERC_MIN :#and car_voltage_mV > VBATT_PAUSE_CHARGING * 1e3:
                 print("log purpose : HARDWARE.set_battery_charging(True)  True ")
                 HARDWARE.set_battery_charging(True)
+                return
