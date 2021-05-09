bf0a9dfbe923899668ad22af572fba327e13f72c
whrho <whrho@sparrowfasoo.com>
2021-05-09 14:00:06 +0900

    fix batt charging

--- selfdrive/thermald/eon_battery_manager.py
+++ selfdrive/thermald/eon_battery_manager.py
@@ -1,28 +1,28 @@
 from selfdrive.thermald.power_monitoring import VBATT_PAUSE_CHARGING
 from selfdrive.hardware import HARDWARE,EON
-BATT_PERC_MIN = 85
-BATT_PERC_MAX = 90
+BATT_PERC_MIN = 40
+BATT_PERC_MAX = 60
 
 def setEONChargingStatus(car_voltage_mV, batteryPercent) :
     if EON:
         if car_voltage_mV is None or batteryPercent is None :
             HARDWARE.set_battery_charging(True)
             return
-        print( "car_voltage_mV:",car_voltage_mV)
-        print( "batteryPercent:",batteryPercent)
-        print( "VBATT_PAUSE_CHARGING:",VBATT_PAUSE_CHARGING)
-        print("VBATT_PAUSE_CHARGING * 1e3:", VBATT_PAUSE_CHARGING * 1e3)
+        # print( "car_voltage_mV:",car_voltage_mV)
+        # print( "batteryPercent:",batteryPercent)
+        # print( "VBATT_PAUSE_CHARGING:",VBATT_PAUSE_CHARGING)
+        # print("VBATT_PAUSE_CHARGING * 1e3:", VBATT_PAUSE_CHARGING * 1e3)
         if HARDWARE.get_battery_charging() :
-            print("log purpose : HARDWARE.get_battery_charging()  True ")
+            # print("log purpose : HARDWARE.get_battery_charging()  True ")
             # if batteryPercent > BATT_PERC_MAX or car_voltage_mV  < VBATT_PAUSE_CHARGING * 1e3 :
             if batteryPercent > BATT_PERC_MAX : #or car_voltage_mV < VBATT_PAUSE_CHARGING * 1e3:
-                print("log purpose : HARDWARE.set_battery_charging(False)  False ")
+                # print("log purpose : HARDWARE.set_battery_charging(False)  False ")
                 HARDWARE.set_battery_charging(False)
                 return
         else :
-            print("log purpose : HARDWARE.get_battery_charging()  False ")
+            # print("log purpose : HARDWARE.get_battery_charging()  False ")
             #if batteryPercent < BATT_PERC_MIN and car_voltage_mV  > VBATT_PAUSE_CHARGING * 1e3:
             if batteryPercent < BATT_PERC_MIN :#and car_voltage_mV > VBATT_PAUSE_CHARGING * 1e3:
-                print("log purpose : HARDWARE.set_battery_charging(True)  True ")
+                # print("log purpose : HARDWARE.set_battery_charging(True)  True ")
                 HARDWARE.set_battery_charging(True)
                 return
