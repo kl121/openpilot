03f260b055153cffd02569c9137997eeaf6d0218
hanabi95 <youngsoo.cha@gmail.com>
2021-05-09 20:39:24 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -39,8 +39,8 @@ class CarInterface(CarInterfaceBase):
 
     if LQR_enabled:
       ret.lateralTuning.init('lqr')
-      ret.lateralTuning.lqr.scale = 1800.0
-      ret.lateralTuning.lqr.ki = 0.04
+      ret.lateralTuning.lqr.scale = 1965.0  #by 양민님
+      ret.lateralTuning.lqr.ki = 0.024
       ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
       ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
       ret.lateralTuning.lqr.c = [1., 0.]
