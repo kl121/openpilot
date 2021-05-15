639fea2f58ca2f06c3df56520683bd093572a050
hanabi95 <youngsoo.cha@gmail.com>
2021-05-15 18:26:37 +0900

    fix

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -131,8 +131,7 @@ static void update_state(UIState *s) {
   UIScene &scene = s->scene;
 
   if (sm.updated("carState")) {
-    auto car_state = sm["carState"].getCarState();
-	  s->scene.brakeLights = scene.car_state.getBrakeLights();
+	  s->scene.brakeLights = sm["carState"].getCarState().getBrakeLights();
   }
   if (sm.updated("radarState")) {
     std::optional<cereal::ModelDataV2::XYZTData::Reader> line;
