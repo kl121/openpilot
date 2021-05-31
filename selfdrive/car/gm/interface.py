5c58d15ace0bab7d5515ba49491029ca2a497c8b
whrho <whrho@sparrowfasoo.com>
2021-05-31 10:51:15 +0900

    just feel.

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -49,7 +49,7 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.init('lqr')
 
     ret.lateralTuning.lqr.scale = 1965.0
-    ret.lateralTuning.lqr.ki = 0.024
+    ret.lateralTuning.lqr.ki = 0.032
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
     ret.lateralTuning.lqr.c = [1., 0.]
