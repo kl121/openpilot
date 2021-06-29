360ca0c87dc8a034f790d8473bc9f4f778637227
Vehicle Researcher <user@comma.ai>
2021-06-29 16:22:36 -0700

    openpilot v0.8.6 release

--- selfdrive/modeld/models/dmonitoring.cc
+++ selfdrive/modeld/models/dmonitoring.cc
@@ -1,4 +1,4 @@
-#include <string.h>
+#include <cstring>
 
 #include "libyuv.h"
 
@@ -6,6 +6,7 @@
 #include "selfdrive/common/params.h"
 #include "selfdrive/common/timing.h"
 #include "selfdrive/hardware/hw.h"
+
 #include "selfdrive/modeld/models/dmonitoring.h"
 
 #define MODEL_WIDTH 320
@@ -19,7 +20,7 @@
 #endif
 
 void dmonitoring_init(DMonitoringModelState* s) {
-  const char *model_path = "../../models/dmonitoring_model_q.dlc";
+  const char *model_path = Hardware::PC() ? "../../models/dmonitoring_model.dlc" : "../../models/dmonitoring_model_q.dlc";
   int runtime = USE_DSP_RUNTIME;
   s->m = new DefaultRunModel(model_path, &s->output[0], OUTPUT_SIZE, runtime);
   s->is_rhd = Params().getBool("IsRHD");
@@ -161,11 +162,13 @@ DMonitoringResult dmonitoring_eval_frame(DMonitoringModelState* s, void* stream_
   ret.partial_face = s->output[35];
   ret.distracted_pose = s->output[36];
   ret.distracted_eyes = s->output[37];
+  ret.eyes_on_road = s->output[38];
+  ret.phone_use = s->output[39];
   ret.dsp_execution_time = (t2 - t1) / 1000.;
   return ret;
 }
 
-void dmonitoring_publish(PubMaster &pm, uint32_t frame_id, const DMonitoringResult &res, float execution_time, kj::ArrayPtr<const float> raw_pred){
+void dmonitoring_publish(PubMaster &pm, uint32_t frame_id, const DMonitoringResult &res, float execution_time, kj::ArrayPtr<const float> raw_pred) {
   // make msg
   MessageBuilder msg;
   auto framed = msg.initEvent().initDriverState();
@@ -187,6 +190,8 @@ void dmonitoring_publish(PubMaster &pm, uint32_t frame_id, const DMonitoringResu
   framed.setPartialFace(res.partial_face);
   framed.setDistractedPose(res.distracted_pose);
   framed.setDistractedEyes(res.distracted_eyes);
+  framed.setEyesOnRoad(res.eyes_on_road);
+  framed.setPhoneUse(res.phone_use);
   if (send_raw_pred) {
     framed.setRawPredictions(raw_pred.asBytes());
   }
