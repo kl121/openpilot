4d987cb7fe984778ddf51f732126b2699804f953
Vehicle Researcher <user@comma.ai>
2021-07-13 23:55:00 -0700

    openpilot v0.8.6 release

---
+++ panda/board/bootstub_declarations.h
@@ -0,0 +1,15 @@
+// ******************** Prototypes ********************
+void puts(const char *a){ UNUSED(a); }
+void puth(uint8_t i){ UNUSED(i); }
+void puth2(uint8_t i){ UNUSED(i); }
+typedef struct board board;
+typedef struct harness_configuration harness_configuration;
+// No CAN support on bootloader
+void can_flip_buses(uint8_t bus1, uint8_t bus2){UNUSED(bus1); UNUSED(bus2);}
+void pwm_init(TIM_TypeDef *TIM, uint8_t channel);
+void pwm_set(TIM_TypeDef *TIM, uint8_t channel, uint8_t percentage);
+void can_set_obd(uint8_t harness_orientation, bool obd){UNUSED(harness_orientation); UNUSED(obd);}
+
+// ********************* Globals **********************
+uint8_t hw_type = 0;
+const board *current_board;
