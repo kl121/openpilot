ad67f3829af621afc5f05e588d7b6636394ab72f
DS5AJD <81415671+ds5ajd@users.noreply.github.com>
2021-09-13 22:15:11 +0900

    Update thermald.py

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -431,7 +431,7 @@ def thermald_thread():
 
     # dp - auto shutdown
     if off_ts is not None:
-      shutdown_sec = 240
+      shutdown_sec = 600
       sec_now = sec_since_boot() - off_ts
       if (shutdown_sec - 5) < sec_now:
         msg.deviceState.chargingDisabled = True
